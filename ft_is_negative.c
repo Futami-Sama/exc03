986cde21bd1ce5ec10fe49c456700cc756925934
Futami-Sama <denambridek@gmail.com>
2022-05-22 11:24:40 +0200

    alpha1

---
+++ ft_is_negative.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int  ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_is_negative(int n)
+{
+    int numbers;
+
+
+}
+  
+   
+
+void main(void)
+{
+    void ft_is_negative(int n);
+}
+
